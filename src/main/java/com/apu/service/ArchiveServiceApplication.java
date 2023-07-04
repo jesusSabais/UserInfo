50b4fce6a5c2364342968a834a32b9059d87dc25
Alan Vargas <alan.vargas@audatex.com>
2023-07-04 15:30:03 -0600

    ops - initial commit with new springboot project files.

---
+++ src/main/java/com/apu/service/ArchiveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apu.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchiveServiceApplication.class, args);
+    }
+
+}
